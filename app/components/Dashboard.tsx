b36c4322348566f8ba4482ff0b05c943d89da5d9
Jai Bhalla <jai.bhalla2021@vitstudent.ac.in>
2025-02-04 19:54:50 +0530

    understanding the links for the admin

--- app/components/Dashboard.tsx
+++ app/components/Dashboard.tsx
@@ -10,12 +10,13 @@ import {
     SheetTrigger,
   } from "@/components/ui/sheet"
 import Logo from './utils/Logo';
+import { Button } from '@/components/ui/button';
   
 
 export default function Dashboard() {
   return (
     <div>
-        <nav className='flex w-full items-center justify-between border-[#444444] border-b p-2'>
+        <nav className='flex w-full items-center justify-between border-[#444444] border-b py-1 px-2'>
             <div>
                 <Logo/>
             </div>
@@ -23,11 +24,22 @@ export default function Dashboard() {
                 <SheetTrigger>
                     <Image src={userImage} alt=''/>
                 </SheetTrigger>
-                <SheetContent side={'left'} className='bg-[#151515] text-white border-none py-2 px-0 font-poppins'>
-                    <div className='flex w-full justify-between items-center border-[#444444] border-b pb-2 pr-10 pl-2'>
-                        <Logo/> 
-                        <div className='font-bold text-xl'>Welcome, Rajiv Bhalla</div>
+                <SheetContent side={'left'} className='bg-[#131313] text-white border-none py-2 px-0 font-poppins flex flex-col justify-between'>
+                    <div>
+                        <div className='flex w-full justify-between items-center border-[#444444] border-b pb-2 pr-10 pl-2'>
+                            <Logo/> 
+                            <div className='text-sm md:text-lg'>Welcome, <i className='not-italic font-bold'>Rajiv Bhalla</i></div>
+                        </div>
+                        <div>
+                            <ul>
+                                <li className='hover:bg-[#202020] transition-all duration-300 py-4 border-b border-[#444444] flex items-center justify-center'>User Management</li>
+                                <li className='hover:bg-[#202020] transition-all duration-300 py-4 border-b border-[#444444] flex items-center justify-center'>Employee Tracking</li>
+                                <li className='hover:bg-[#202020] transition-all duration-300 py-4 border-b border-[#444444] flex items-center justify-center'>Regional Performance</li>
+                                <li className='hover:bg-[#202020] transition-all duration-300 py-4 border-b border-[#444444] flex items-center justify-center'>System & Security</li>
+                            </ul>
+                        </div>
                     </div>
+                    <Button variant="outline" className='mx-auto w-48 bg-[#151515]'>Log Out</Button>
                 </SheetContent>
             </Sheet>
         </nav>
